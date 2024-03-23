3249a653a1ce619d107969de3f3a9d3f8f049b3a
Prem Pratap Singh <prem.pratap@icloud.com>
2024-03-23 23:32:37 +0530

    Add UI navigation flows for search notes, notes list view with mock data

--- AudioNotes/Views/NotesListView.swift
+++ AudioNotes/Views/NotesListView.swift
@@ -17,9 +17,123 @@ struct NotesListView: View {
     
     var notes: [NoteModel]
     
+    // MARK: - Private Properties
+    
+    @State private var shouldShowAddNewNoteView = false
+    @State private var shouldShowSearchNoteView = false
+    
     // MARK: - User Interface
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ZStack {
+                
+                // Main Content
+                VStack(alignment: .center, spacing: 16) {
+                    
+                    // Tool bar
+                    HStack {
+                        
+                        Text(NSLocalizedString("My Notes", comment: "Notes list view - title"))
+                            .font(.system(size: 24))
+                            .foregroundColor(Color.blue)
+                        
+                        Spacer()
+                        
+                        // Record new note button
+                        Button(
+                            action: {
+                                self.shouldShowAddNewNoteView = true
+                            },
+                            label: {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 6)
+                                        .fill(Color.white)
+                                        .frame(width: 40, height: 40)
+                                    Image("micIconNormal")
+                                        .renderingMode(.template)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 25, height: 25)
+                                        .tint(Color.black)
+                                }
+                            }
+                        )
+                        
+                        // Search notes button
+                        Button(
+                            action: {
+                                self.shouldShowSearchNoteView = true
+                            },
+                            label: {
+                                ZStack {
+                                    Image("searchIcon")
+                                        .renderingMode(.template)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 25, height: 25)
+                                        .tint(Color.black)
+                                }
+                            }
+                        )
+                        
+                        
+                    }
+                    .padding(.horizontal, 24)
+                    .padding(.bottom, 40)
+                    
+                    // Notes list
+                    
+                    ScrollView {
+                        LazyVStack(alignment: .leading, spacing: 16) {
+                            ForEach(self.notes, id: \.id) { note in
+                                ZStack(alignment: .leading) {
+                                    RoundedRectangle(cornerRadius: 8)
+                                        .fill(Color.gray)
+                                        .frame(height: 80)
+                                    
+                                    VStack(alignment: .leading, spacing: 8) {
+                                        Text("\(note.dateOfCreation.description)")
+                                            .font(.system(size: 12))
+                                            .foregroundColor(Color.black)
+                                        
+                                        Text("\(note.text)")
+                                            .font(.system(size: 16))
+                                            .foregroundColor(Color.black)
+                                    }
+                                    .padding(.all, 10)
+                                }
+                            }
+                        }
+                    }
+                    .padding(.horizontal, 24)
+                    .padding(.bottom, 24)
+                }
+            }
+            .navigationDestination(
+                isPresented: self.$shouldShowAddNewNoteView,
+                destination: {
+                    AddNewNoteView(delegate: self)
+                        .navigationBarHidden(true)
+                }
+            )
+            .navigationDestination(
+                isPresented: self.$shouldShowSearchNoteView,
+                destination: {
+                    SearchNotesView()
+                        .navigationBarHidden(true)
+                }
+            )
+        }
+    }
+}
+
+
+// MARK: - AddNewNoteViewDelegate delegate methods
+
+extension NotesListView: AddNewNoteViewDelegate {
+    func didSaveNewNote() {
+        self.shouldShowAddNewNoteView = false
+        
     }
 }
--- AudioNotes/Views/SearchNotesView.swift
+++ AudioNotes/Views/SearchNotesView.swift
@@ -12,7 +12,51 @@ import SwiftUI
  list of matching notes.
  */
 struct SearchNotesView: View {
+    
+    // MARK: - Private properties
+    
+    @SwiftUI.Environment(\.presentationMode) private var presentationMode
+    
+    // MARK: - User Interface
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+            // Main Content
+            VStack(alignment: .center, spacing: 24) {
+                HStack {
+                    // Back button
+                    Button(
+                        action: {
+                            self.presentationMode.wrappedValue.dismiss()
+                        },
+                        label: {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 6)
+                                    .fill(Color.white)
+                                    .frame(width: 40, height: 40)
+                                Image("leftArrowIcon")
+                                    .renderingMode(.template)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 20, height: 37)
+                                    .tint(Color.black)
+                            }
+                        }
+                    )
+                    
+                    Spacer()
+                }
+                .padding(.horizontal, 12)
+                .padding(.bottom, 100)
+                
+                Text(NSLocalizedString("Search Notes", comment: "Search notes view - title"))
+                    .font(.system(size: 18))
+                    .foregroundColor(Color.black)
+                    .padding(.horizontal, 24)
+                
+                Spacer()
+            }
+        }
     }
 }
